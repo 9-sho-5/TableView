67d16b4c737e6a697808fe27f8d472d2ae03dd08
9-sho-5 <hosho.kusu@icloud.com>
2022-11-14 10:32:42 +0900

    fix: set tableview

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    var array = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        array = ["Dog","Cat","Cow","Mouse","Rat","Tiger","Bird"]
     }
 
+    // セルを表示する個数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    // Tableセルの表示内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = array[indexPath.row]
+        return cell! // テキストなどを持たせたcellを表示する
+
+    }
 
 }
 
