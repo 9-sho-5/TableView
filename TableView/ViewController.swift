11dbf4a7acf4b42ba48fc8c3ce2539ebcd7fc5e9
9-sho-5 <hosho.kusu@icloud.com>
2022-11-14 10:35:27 +0900

    fix: set edit function

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         array = ["Dog","Cat","Cow","Mouse","Rat","Tiger","Bird"]
+        // NavigationBarの右に「editButton」を追加する
+        navigationItem.rightBarButtonItems = [editButtonItem]
     }
 
     // セルを表示する個数
@@ -29,7 +31,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         cell?.textLabel?.text = array[indexPath.row]
         return cell! // テキストなどを持たせたcellを表示する
+    }
+    
+    // 編集モードに切り替える処理(編集できるようにする)
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.setEditing(editing, animated: animated)
+        tableView.isEditing = editing
+    }
+    
+    //並び替え時の処理
+    func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let array_item = array[fromIndexPath.row]
+        array.remove(at: fromIndexPath.row)
+        array.insert(array_item, at: to.row)
+    }
 
+    //並び替えを可能にする
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
 
 }
